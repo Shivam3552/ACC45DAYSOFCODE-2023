6d7ecc4e1794dd0bea2a9771220f35432c59e961
Shivam3552 <116377775+Shivam3552@users.noreply.github.com>
2023-09-26 10:41:13 +0530

    Create Day9.cpp

---
+++ Day9.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int
+main ()
+{
+  
+int t;
+  
+cin >> t;
+  
+int i = 0;
+  
+while (i < t)
+    {
+      
+int A1,A2,A3,A4,A5,P;
+int time=(A1+A2+A3+A4+A5)*P;
+if(time>120){
+    cout<<"YES"<<endl;
+}
+else{
+    cout<<"NO"<<endl;
+}
+
+i++;
+      
+
+}
+
+}
