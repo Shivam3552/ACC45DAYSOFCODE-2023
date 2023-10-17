69dc51be7af79ef1180df4c644d5ea46f978ce7b
Shivam3552 <116377775+Shivam3552@users.noreply.github.com>
2023-10-17 19:44:21 +0530

    Create d24.cpp

---
+++ d24.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int
+main ()
+{
+  
+int t;
+  
+cin >> t;
+  
+int i = 0;
+  
+while (i < t)
+    {
+  int Y;
+  cin>>Y;
+  if(Y==0){
+      cout<<1<<endl;
+  }
+  else{
+  cout<<3*Y<<endl;
+  
+
+}
+i++;
+}
+
+}
