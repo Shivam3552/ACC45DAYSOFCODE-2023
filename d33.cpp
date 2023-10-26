e5ae47bd0535d97e0fffd566fe246a54d675619a
Shivam3552 <116377775+Shivam3552@users.noreply.github.com>
2023-10-26 19:48:03 +0530

    Create d33.cpp

---
+++ d33.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int
+main ()
+{
+  
+int t;
+  
+cin >> t;
+  
+int i = 0;
+  
+while (i < t)
+    {
+      
+int n,a,b;
+cin>>n>>a>>b;
+cout<<2*(180+n)-(a+b)<<endl;
+ i++;     
+
+}
+
+}
