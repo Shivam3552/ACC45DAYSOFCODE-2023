748603bf1e65ff1224d2a1b8369ad7289ad9fa0c
Shivam3552 <116377775+Shivam3552@users.noreply.github.com>
2023-11-06 09:45:19 +0530

    Create day41.cpp

---
+++ day41.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int
+main ()
+{
+  
+int t;
+  
+cin >> t;
+  
+int i = 0;
+  
+while (i < t)
+    {
+int n,x,d;
+cin>>n>>x>>d;
+cout<<n/(x*5)+d<<endl;;
+i++;
+      
+
+}
+
+}
